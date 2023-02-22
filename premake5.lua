62581ebbc916939dcc8d21d65461d36f935b66a8
darkhole22 <damiano.guida22@gmail.com>
2023-02-22 13:24:10 +0100

    Updated premake5

--- premake5.lua
+++ premake5.lua
@@ -48,15 +48,7 @@ project "GLFW"
 
 		files
 		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"src/**.c"
 		}
 
 		defines 
